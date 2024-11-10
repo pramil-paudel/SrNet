f5a37aac76aa1fc8d3227d0c49ac71aee12999cd
pramil-paudel <ppaudel35@gmail.com>
2024-11-09 20:23:55 -0600

    intel sh

--- SRNET_train.sh
+++ SRNET_train.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH -p intel
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 1
-#SBATCH --mem=1G
-#SBATCH -t 00:20:00
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres="gpu:k20:1"
+#SBATCH -c 4
+#SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH -J SRNET
 #SBATCH -o slurm-%j.out
 
-python3 main.py --cover_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/cover_train --stego_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/container_train/ --valid_cover_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/cover_validation --valid_stego_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/container_validation
+python3 train.py --cover_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/cover_train --stego_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/container_train/ --valid_cover_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/cover_validation --valid_stego_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/container_validation
 
