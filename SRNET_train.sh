933154b3e4585b5bacfbca1ad3b455f0e725af1b
pramil-paudel <ppaudel35@gmail.com>
2024-11-14 16:39:05 -0600

    trying to run SR-net

--- SRNET_train.sh
+++ SRNET_train.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gres="gpu:k20:1"
+#SBATCH --gres="gpu:titanxp:1"
 #SBATCH -c 4
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
-#SBATCH -J SRNET
+#SBATCH -J SRNet
 #SBATCH -o slurm-%j.out
 
 python3 train.py --cover_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/cover_train --stego_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/container_train/ --valid_cover_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/cover_validation --valid_stego_path /scratch/p522p287/DATA/STEN_DATA/IMAGE_NET_OUT/SRNET/container_validation
-
